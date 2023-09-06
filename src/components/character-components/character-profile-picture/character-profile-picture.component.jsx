48e818a8e8a26001aea6ade4c2d292cf5eda77b8
Jason Cameron <dev.jasoncameron@gmail.com>
2023-09-06 06:19:38 -0500

    add raveens pix using url addres from pinterest

--- src/components/character-components/character-profile-picture/character-profile-picture.component.jsx
+++ src/components/character-components/character-profile-picture/character-profile-picture.component.jsx
@@ -3,10 +3,20 @@ import './character-profile-picture.styles.css'
 const CharacterProfilePicture = () => {
 
     return (
-        <div className="h-24 w-24 bg-red-900 rounded-full border-4 border-amber-100">
+        <div>
             {/*  Image Here  */}
+            <img
+                src="https://i.pinimg.com/564x/9e/5f/90/9e5f9074e1378adbc460f31616c0dbaf.jpg"
+                alt="Avatar Image"
+                className="w-24 h-24 rounded-xl"/>
         </div>
     )
 };
 
 export default CharacterProfilePicture;
+
+
+// <div className="relative">
+//     <img className="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="">
+//         <span className="top-0 left-7 absolute  w-3.5 h-3.5 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full"></span>
+// </div>
