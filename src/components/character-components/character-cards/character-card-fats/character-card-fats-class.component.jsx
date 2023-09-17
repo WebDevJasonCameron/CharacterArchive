f716480f31298a61cbe520f36074632117e38a3a
Jason Cameron <dev.jasoncameron@gmail.com>
2023-09-17 16:41:37 -0500

    working fat card

--- src/components/character-components/character-cards/character-card-fats/character-card-fats-class.component.jsx
+++ src/components/character-components/character-cards/character-card-fats/character-card-fats-class.component.jsx
@@ -3,13 +3,16 @@ import './character-card-fats-class.styles.css'
 
 // COMP
 const CardFatsClass = ({ character }) => {
+
+    const traitsRemod = character.cFATsClassTraits
+
     return (
         <div className="col-start-3 row-start-2 row-span-5 flex flex-col text-xl m-2 border-4 border-slate-600 rounded-3xl">
             {/* Title */}
             <h3 className="text-center text-2xl text-slate-400 my-2">Features & Traits</h3>
             {/* Class */}
             <div className="p-3">
-                {character.cFATsClassTraits}
+                {traitsRemod}
             </div>
         </div>
     )
