cf8ae8bbf22af1e8d32cc82cbf4d34ce43eeef0a
Jason Cameron <dev.jasoncameron@gmail.com>
2023-09-18 20:16:28 -0500

    fat card

--- src/components/character-components/character-cards/character-card-fats/character-card-fats-class.component.jsx
+++ src/components/character-components/character-cards/character-card-fats/character-card-fats-class.component.jsx
@@ -13,19 +13,23 @@ const CardFatsClass = ({ character }) => {
     )
 
     const FatClassFeature = ({ featureName, featureSource, featureMod, featureDescription }) => (
-        <div>
-            <h6 className="text-xl text-slate-400">
-                {featureName}
-            </h6>
-            <small className="text-sm text-slate-400">
-                {featureSource}
-            </small>
+        <div className="flex flex-col gap-1 px-3 group">
+            <div className="flex py-1 gap-6 justify-between">
+                <h6 className="text-xl text-slate-400">
+                    {featureName}
+                </h6>
+                <small className="text-sm text-slate-600">
+                    {featureSource}
+                </small>
+            </div>
+
             <div>
-                {featureMod !== ""? <p>{featureMod}</p> : ""}
+                {featureMod !== ""? <p className="text-red-400 group-hover:text-red-200">{featureMod}</p> : ""}
             </div>
             <div>
-                {featureDescription !== ""? <p>{featureDescription}</p> : ""}
+                {featureDescription !== ""? <p className="text-slate-400 group-hover:text-slate-100">{featureDescription}</p> : ""}
             </div>
+            <hr className="border-slate-600"/>
         </div>
     )
 
@@ -40,7 +44,7 @@ const CardFatsClass = ({ character }) => {
     return (
         <div className="col-start-3 row-start-2 row-span-5 flex flex-col text-xl m-2 border-4 border-slate-600 rounded-3xl">
             {/* Title */}
-            <h3 className="text-center text-2xl text-slate-400 my-2">Features & Traits</h3>
+            <h3 className="text-center text-2xl text-slate-400 my-2">Class Features & Traits</h3>
             {/* Class */}
             <div className="p-3">
 
