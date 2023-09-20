eeab7f97baae15ca6e04f7ecc77d1823fd2e0919
Jason Cameron <dev.jasoncameron@gmail.com>
2023-09-19 20:32:51 -0500

    class fat works! maps are cool

--- src/components/character-components/character-cards/character-card-fats/character-card-fats-class.component.jsx
+++ src/components/character-components/character-cards/character-card-fats/character-card-fats-class.component.jsx
@@ -1,16 +1,23 @@
 // Style
 import './character-card-fats-class.styles.css'
+import {findAllByDisplayValue} from "@testing-library/react";
 
 // COMP
 const CardFatsClass = ({ character }) => {
 
-    const fatClassName = ({ className }) => (
-        <h5 className="text-2xl text-slate-400">
-            {className}
-        </h5>
-    )
 
-    const FatClassFeature = ({ featureName, featureSource, featureMod, featureDescription }) => (
+    // Html
+    const classHtml = ({ className, classFeatures }) => (
+        <div>
+            <h5 className="text-2xl text-slate-400">
+            {className}
+            </h5>
+            <div>
+                { mapFeatures(classFeatures) }
+            </div>
+        </div>
+    )
+    const featureHtml = ({ featureName, featureSource, featureMod, featureDescription }) => (
         <div className="flex flex-col gap-1 px-3 group">
             <div className="flex py-1 gap-6 justify-between">
                 <h6 className="text-xl text-slate-400">
@@ -31,23 +38,18 @@ const CardFatsClass = ({ character }) => {
         </div>
     )
 
-    const FeatureList = (feature) => (
+    // Maps
+    const mapClasses = ( character ) => (
         <div>
-            {character.cFATsClassTraits[0].classFeatures.map(FatClassFeature)}
+            { character.cFATsClassTraits.map(classHtml) }
+        </div>
+    )
+    const mapFeatures = (features) => (
+        <div>
+            {features.map(featureHtml)}
         </div>
     )
 
-    const fatOutput = ({ character }) => {
-
-        let output = {}
-
-        for (let i = 0; i < character.cFATsClassTraits.length; i++) {
-
-            output += character.cFATsClassTraits[i].className
-
-        }
-
-    }
 
     return (
         <div className="col-start-3 row-start-2 row-span-5 flex flex-col text-xl m-2 border-4 border-slate-600 rounded-3xl">
@@ -56,10 +58,11 @@ const CardFatsClass = ({ character }) => {
             {/* Class */}
             <div className="p-3">
 
+                {mapClasses(character)}
 
             </div>
             <div>
-                { FeatureList()}
+                {/*{ mapFeatures(character.cFATsClassTraits[0].classFeatures) }*/}
             </div>
 
         </div>
