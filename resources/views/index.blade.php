d2a82452a2aa15c956278fc31a4a925b758a2083
Jason Cameron <dev.jasoncameron@gmail.com>
2025-03-30 20:50:36 -0500

    really getting the spells in

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -113,6 +113,292 @@
 
                 {{--Details Section--}}
                 <section id="details-section">
+                    {{--Detail -> Spells--}}
+                    {{--Cantrips--}}
+                    <div id="detail-spells" class="">
+                        <h5 class="text-xs text-gray-400 m-2 pt-2">
+                            Cantrips
+                        </h5>
+                        <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs p-2">
+                            <div class="relative overflow-x-auto">
+                                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                        <tr>
+                                            <th scope="col" class="px-6 py-3">
+                                                Name
+                                            </th>
+                                            <th scope="col" class="px-6 py-3">
+                                                Time
+                                            </th>
+                                            <th scope="col" class="px-6 py-3">
+                                                Range
+                                            </th>
+                                            <th scope="col" class="px-6 py-3">
+                                                Hit/DC
+                                            </th>
+                                            <th scope="col" class="px-6 py-3">
+                                                Effect
+                                            </th>
+                                            <th scope="col" class="px-6 py-3">
+                                                Notes
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="text-xs">
+                                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                                            {{--Name--}}
+                                            <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                Mending
+                                            </th>
+                                            {{--Time--}}
+                                            <td class="px-6 py-4">
+                                                1m
+                                            </td>
+                                            {{--Range--}}
+                                            <td class="px-6 py-4">
+                                                Touch
+                                            </td>
+                                            {{--Hit/DC--}}
+                                            <td class="px-6 py-4">
+                                                --
+                                            </td>
+                                            {{--Effect--}}
+                                            <td class="px-6 py-4">
+                                                Utility
+                                            </td>
+                                            {{--Notes--}}
+                                            <td class="px-6 py-4">
+                                                V,S,M
+                                            </td>
+                                        </tr>
+                                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                                            {{--Name--}}
+                                            <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                Mold Earth
+                                            </th>
+                                            {{--Time--}}
+                                            <td class="px-6 py-4">
+                                                1A
+                                            </td>
+                                            {{--Range--}}
+                                            <td class="px-6 py-4">
+                                                30 ft.
+                                            </td>
+                                            {{--Hit/DC--}}
+                                            <td class="px-6 py-4">
+                                                --
+                                            </td>
+                                            {{--Effect--}}
+                                            <td class="px-6 py-4">
+                                                Control
+                                            </td>
+                                            {{--Notes--}}
+                                            <td class="px-6 py-4">
+                                                5ft. <i class="fa-solid fa-cube"></i>, S
+                                            </td>
+                                        </tr>
+                                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                                            {{--Name--}}
+                                            <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                Spare the Dying
+                                            </th>
+                                            {{--Time--}}
+                                            <td class="px-6 py-4">
+                                                1A
+                                            </td>
+                                            {{--Range--}}
+                                            <td class="px-6 py-4">
+                                                15 ft.
+                                            </td>
+                                            {{--Hit/DC--}}
+                                            <td class="px-6 py-4">
+                                                --
+                                            </td>
+                                            {{--Effect--}}
+                                            <td class="px-6 py-4">
+                                                Healing
+                                            </td>
+                                            {{--Notes--}}
+                                            <td class="px-6 py-4">
+                                                V,S
+                                            </td>
+                                        </tr>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+                    {{--1st Level--}}
+                    <div id="detail-spells" class="">
+                        <div class="flex justify-between">
+                            <h5 class="text-xs text-gray-400 m-2 pt-2">
+                                1st Level
+                            </h5>
+                            <div class="my-2 mr-2">
+                                <input class="accent-gray-400/25" type="checkbox" checked />
+                                <input class="accent-gray-400/25" type="checkbox" checked />
+                            </div>
+                        </div>
+
+                        {{--Spell Card 1--}}
+                        <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs p-2">
+                            <div class="relative overflow-x-auto">
+                                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                        <tr>
+                                            <th scope="col" class="px-6 py-3">
+                                                Name
+                                            </th>
+                                            <th scope="col" class="px-6 py-3">
+                                                Time
+                                            </th>
+                                            <th scope="col" class="px-6 py-3">
+                                                Range
+                                            </th>
+                                            <th scope="col" class="px-6 py-3">
+                                                Hit/DC
+                                            </th>
+                                            <th scope="col" class="px-6 py-3">
+                                                Comp
+                                            </th>
+                                            <th scope="col" class="px-6 py-3">
+                                                Dur
+                                            </th>
+                                            <th scope="col" class="px-6 py-3">
+                                                Effect
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="text-xs">
+                                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                                            {{--Name--}}
+                                            <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                Cure Wounds
+                                            </th>
+                                            {{--Time--}}
+                                            <td class="px-6 py-4">
+                                                1A
+                                            </td>
+                                            {{--Range--}}
+                                            <td class="px-6 py-4">
+                                                Touch
+                                            </td>
+                                            {{--Hit/DC--}}
+                                            <td class="px-6 py-4">
+                                                --
+                                            </td>
+                                            {{--Components--}}
+                                            <td class="px-6 py-4">
+                                                V,S
+                                            </td>
+                                            {{--Duration--}}
+                                            <td class="px-6 py-4">
+                                                Inst.
+                                            </td>
+                                            {{--Effect--}}
+                                            <td class="px-6 py-4">
+                                                2d8+3 <i class="fa-solid fa-heart"></i>
+                                            </td>
+                                        </tr>
+                                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                                            {{--Name--}}
+                                            <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                Elevated Sight
+                                            </th>
+                                            {{--Time--}}
+                                            <td class="px-6 py-4">
+                                                1A
+                                            </td>
+                                            {{--Range--}}
+                                            <td class="px-6 py-4">
+                                                Self
+                                            </td>
+                                            {{--Hit/DC--}}
+                                            <td class="px-6 py-4">
+                                                --
+                                            </td>
+                                            {{--Components--}}
+                                            <td class="px-6 py-4">
+                                                V,S
+                                            </td>
+                                            {{--Duration--}}
+                                            <td class="px-6 py-4">
+                                                Con.
+                                            </td>
+                                            {{--Effect--}}
+                                            <td class="px-6 py-4">
+
+                                            </td>
+                                        </tr>
+                                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                                            {{--Name--}}
+                                            <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                Entangle
+                                            </th>
+                                            {{--Time--}}
+                                            <td class="px-6 py-4">
+                                                1A
+                                            </td>
+                                            {{--Range--}}
+                                            <td class="px-6 py-4">
+                                                90 ft.
+                                            </td>
+                                            {{--Hit/DC--}}
+                                            <td class="px-6 py-4">
+                                                STR 13
+                                            </td>
+                                            {{--Components--}}
+                                            <td class="px-6 py-4">
+                                                1m, 20ft., V/S
+                                            </td>
+                                            {{--Duration--}}
+                                            <td class="px-6 py-4">
+                                                Con.
+                                            </td>
+                                            {{--Effect--}}
+                                            <td class="px-6 py-4">
+                                                Restrained
+                                            </td>
+                                        </tr>
+                                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                                            {{--Name--}}
+                                            <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                Faerie Fire <i class="fa-solid fa-copyright font-medium text-gray-600"></i>
+                                            </th>
+                                            {{--Time--}}
+                                            <td class="px-6 py-4">
+                                                1A
+                                            </td>
+                                            {{--Range--}}
+                                            <td class="px-6 py-4">
+                                                60 ft./20 ft. <i class="fa-solid fa-cube"></i>
+                                            </td>
+                                            {{--Hit/DC--}}
+                                            <td class="px-6 py-4">
+                                                DEX 13
+                                            </td>
+                                            {{--Effect--}}
+                                            <td class="px-6 py-4">
+                                                V
+                                            </td>
+                                            {{--Note--}}
+                                            <td class="px-6 py-4">
+                                                Con.
+                                            </td>
+                                            {{--Effect--}}
+                                            <td class="px-6 py-4">
+                                                Invisible
+                                            </td>
+                                        </tr>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
 
                 </section>
 
