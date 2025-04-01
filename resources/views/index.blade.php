2cf068e753853d5264a89fdfb773f0a786bb3e09
Jason Cameron <dev.jasoncameron@gmail.com>
2025-04-01 04:27:02 -0500

    still blocking everthing together

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -40,185 +40,124 @@
         {{--Main--}}
         <main class="container flex flex-col mx-auto p-4 mt-4 space-x-2">
 
-            <div id="character-attributes" class="flex flex-row bg-gray-500 y-400 basic-1/3 p-2">
-                {{--Top Banner--}}
-                <div id="top-banner" class="flex flex-row justify-between items-center">
+            {{--Top Banner--}}
+            <div id="top-banner" class="flex flex-row justify-between items-center bg-gray-500 p-2">
 
-                    {{--Character Profile--}}
-                    <div id="character-profile" class="flex flex-row text-sm mr-4">
-                        <div class="mx-4">
-                            <img class="w-22 rounded-full overflow-hidden border-4 border-gray-300 shadow-lg"
-                                 src="{{ asset('images/roskat_head_image.png') }}"
-                                 alt="logo"/>
-                        </div>
-                        <ul  class="mx-4">
-                            <li class="text-xs"><strong class="text-red-300">Name:</strong> Roskat</li>
-                            <li class="text-xs"><strong class="text-red-300">Species:</strong> Procyon</li>
-                            <li class="text-xs"><strong class="text-red-300">Classes:</strong> Rogue 1 / Druid 1</li>
-                            <li class="text-xs"><strong class="text-red-300">Background:</strong> Haunted One</li>
-                        </ul>
-                        <ul class="mx-4">
-                            <li class="text-xs"><strong class="text-red-300">HP:</strong> 17</li>
-                            <li class="text-xs"><strong class="text-red-300">AC:</strong> 15</li>
-                            <li class="text-xs"><strong class="text-red-300">Init:</strong> +4</li>
-                        </ul>
-                        <ul class="mx-4">
-                            <li class="text-xs"><strong class="text-red-300">Hit Dice:</strong> 1d8 + 1d8</li>
-                            <li class="text-xs"><strong class="text-red-300">Prof. Bonus:</strong> +2</li>
-                            <li class="text-xs"><strong class="text-red-300">Ability Saves:</strong> -</li>
-                            <li class="text-xs"><strong class="text-red-300">XP:</strong> Milestones</li>
-                        </ul>
-                        <ul class="mx-4">
-                            <li class="text-xs"><strong class="text-red-300">Level:</strong> 2</li>
-                        </ul>
-
+                {{--Character Profile--}}
+                <div id="character-profile" class="flex text-sm mr-4">
+                    <div class="mx-4">
+                        <img class="w-22 rounded-full overflow-hidden border-4 border-gray-300 shadow-lg"
+                             src="{{ asset('images/roskat_head_image.png') }}"
+                             alt="logo"/>
                     </div>
-
-                    {{--View Mode Buttons--}}
-                    <div id="view-mode-buttons" class="flex flex-col">
-                        <div class="flex flex-row justify-between">
-                            <button
-                                type="button"
-                                class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-xs px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
-                                    Dark
-                            </button>
-                            <button
-                                type="button"
-                                class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-xs px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
-                                    Stats
-                            </button>
-                            <button
-                                type="button"
-                                class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-xs px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
-                                    Journal
-                            </button>
-                            <button
-                                type="button"
-                                class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-xs px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
-                                    Combat
-                            </button>
-                        </div>
-                        <div class="flex flex-row justify-between">
-                            <button
-                                type="button"
-                                class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-xs px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
-                                    Inventory
-                            </button>
-                            <button
-                                type="button"
-                                class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-xs px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
-                                    Relations
-                            </button>
-                            <button
-                                type="button"
-                                class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-xs px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
-                                    Spells
-                            </button>
-                            <button
-                                type="button"
-                                class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-xs px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
-                                    Art
-                            </button>
-                        </div>
-                    </div>
+                    <ul  class="mx-4">
+                        <li class="text-xs"><strong class="text-red-300">Name:</strong> Roskat</li>
+                        <li class="text-xs"><strong class="text-red-300">Species:</strong> Procyon</li>
+                        <li class="text-xs"><strong class="text-red-300">Classes:</strong> Rogue 1 / Druid 1</li>
+                        <li class="text-xs"><strong class="text-red-300">Background:</strong> Haunted One</li>
+                    </ul>
+                    <ul class="mx-4">
+                        <li class="text-xs"><strong class="text-red-300">HP:</strong> 17</li>
+                        <li class="text-xs"><strong class="text-red-300">AC:</strong> 15</li>
+                        <li class="text-xs"><strong class="text-red-300">Init:</strong> +4</li>
+                    </ul>
+                    <ul class="mx-4">
+                        <li class="text-xs"><strong class="text-red-300">Hit Dice:</strong> 1d8 + 1d8</li>
+                        <li class="text-xs"><strong class="text-red-300">Prof. Bonus:</strong> +2</li>
+                        <li class="text-xs"><strong class="text-red-300">Ability Saves:</strong> -</li>
+                        <li class="text-xs"><strong class="text-red-300">XP:</strong> Milestones</li>
+                    </ul>
+                    <ul class="mx-4">
+                        <li class="text-xs"><strong class="text-red-300">Level:</strong> 2</li>
+                    </ul>
 
                 </div>
+
+                {{--View Mode Buttons--}}
+                <div id="view-mode-buttons" class="flex flex-col">
+                    <div class="flex flex-row justify-between">
+                        <button
+                            type="button"
+                            class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-xs px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                                Dark
+                        </button>
+                        <button
+                            type="button"
+                            class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-xs px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                                Stats
+                        </button>
+                        <button
+                            type="button"
+                            class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-xs px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                                Journal
+                        </button>
+                        <button
+                            type="button"
+                            class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-xs px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                                Combat
+                        </button>
+                    </div>
+                    <div class="flex flex-row justify-between">
+                        <button
+                            type="button"
+                            class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-xs px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                                Inventory
+                        </button>
+                        <button
+                            type="button"
+                            class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-xs px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                                Relations
+                        </button>
+                        <button
+                            type="button"
+                            class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-xs px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                                Spells
+                        </button>
+                        <button
+                            type="button"
+                            class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-xs px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                                Art
+                        </button>
+                    </div>
+                </div>
+
             </div>
 
-            <div id="character-details"  class="bg-gray-600 basic-2/3">
-                {{--CS Filter Tabs--}}
-                <div
-                    id="cs-filter-tabs"
-                    class="flex flex-wrap -mb-px text-sm font-medium text-center"
-                    data-tabs-toggle="#cs-tab-content"
-                    role="tablist">
-                    <ul class="flex space-x-6 mr-6 text-xs">
-                        <li class="me-2" role="presentation">
-                            <button
-                                class="inline-block p-4 border-b-2 rounded-t-lg"
-                                id="spell-tab"
-                                data-tabs-target="#spells"
-                                type="button"
-                                role="tab"
-                                aria-controls="spells"
-                                aria-selected="true">
-                                    Spells
-                            </button>
-                        </li>
-                        <li class="me-2" role="presentation">
-                            <button
-                                class="inline-block p-4 border-b-2 rounded-t-lg"
-                                id="stats-tab"
-                                data-tabs-target="#stats"
-                                type="button"
-                                role="tab"
-                                aria-controls="stats"
-                                aria-selected="false">
-                                    Stats
-                            </button>
-                        </li>
-                        <li class="me-2" role="presentation">
-                            <button
-                                class="inline-block p-4 border-b-2 rounded-t-lg"
-                                id="combat-tab"
-                                data-tabs-target="#combat"
-                                type="button"
-                                role="tab"
-                                aria-controls="combat"
-                                aria-selected="false">
-                                    Combat
-                            </button>
-                        </li>
-                        <li class="me-2" role="presentation">
-                            <button
-                                class="inline-block p-4 border-b-2 rounded-t-lg"
-                                id="inventory-tab"
-                                data-tabs-target="#inventory"
-                                type="button"
-                                role="tab"
-                                aria-controls="inventory"
-                                aria-selected="false">
-                                    Inventory
-                            </button>
-                        </li>
-                        <li class="me-2" role="presentation">
-                            <button
-                                class="inline-block p-4 border-b-2 rounded-t-lg"
-                                id="class-features-tab"
-                                data-tabs-target="#class-features"
-                                type="button"
-                                role="tab"
-                                aria-controls="class-features"
-                                aria-selected="false">
-                                    Class Features
-                            </button>
-                        </li>
-                        <li class="me-2" role="presentation">
-                            <button
-                                class="inline-block p-4 border-b-2 rounded-t-lg"
-                                id="species-features-tab"
-                                data-tabs-target="#species-features"
-                                type="button"
-                                role="tab"
-                                aria-controls="species-features"
-                                aria-selected="false">
-                                    Species Features
-                            </button>
-                        </li>
-                        <li class="me-2" role="presentation">
-                            <button
-                                class="inline-block p-4 border-b-2 rounded-t-lg"
-                                id="feats-tab"
-                                data-tabs-target="#feats"
-                                type="button"
-                                role="tab"
-                                aria-controls="feats"
-                                aria-selected="false">
-                                    Feats
-                            </button>
-                        </li>
-                    </ul>
+            {{--Views--}}
+            <div id="views">
+
+                {{--View Mode Filter Tabs--}}
+                <div class="md:flex">
+                    <div class="flex-column space-y space-y-4 text-xs font-medium text-gray-500 dark:text-gray-400 md:me-4 mb-4 md:mb-0">
+                        <div>
+                            <a href="#" class="inline-flex items-center px-4 py-1 mt-1 text-white bg-blue-700 rounded-lg active w-full dark:bg-blue-600" aria-current="page">
+                                <i class="fa-solid fa-chart-simple me-2"></i>
+                                Stats
+                            </a>
+
+                        </div>
+                        <div>
+                            <a href="#" class="inline-flex items-center px-4 py-1 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white">
+                                <i class="fa-solid fa-wand-sparkles me-2"></i>
+                                Spells
+                            </a>
+                        </div>
+
+                    </div>
+                    <div class="p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 rounded-lg w-full">
+                        <h3 class="text-lg font-bold text-gray-900 dark:text-white mb-2">Profile Tab</h3>
+                        <p class="mb-2">This is some placeholder content the Profile tab's associated content, clicking another tab will toggle the visibility of this one for the next.</p>
+                        <p>The tab JavaScript swaps classes to control the content visibility and styling.</p>
+                    </div>
                 </div>
+
+
+
+
+
+
+
+
 
                 {{--CS Tab Content--}}
                 <tab id="cs-tab-content">
