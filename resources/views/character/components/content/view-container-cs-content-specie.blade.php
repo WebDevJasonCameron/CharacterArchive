a06b3dd626ce4e6f2db184ee7eb7467e27b9de54
Jason Cameron <dev.jasoncameron@gmail.com>
2025-04-14 18:15:49 -0500

    update spec

--- resources/views/character/components/content/view-container-cs-content-specie.blade.php
+++ resources/views/character/components/content/view-container-cs-content-specie.blade.php
@@ -14,126 +14,187 @@
     </div>
 
     {{--Content--}}
-    {{--Ability Score Increase--}}
-    <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs p-2">
-        <p>
-            <strong class="text-lg">Ability Score Increase </strong>
-            <span class="text-xs text-gray-400 m-2 pt-2">
-                HCS, pg. 27
-            </span>
-        </p>
-        <p>
-            Your Wisdom score increases by 2, and your Constitution score increases by 1.
-        </p>
-    </div>
-    {{--Age--}}
-    <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs mt-4 p-2">
-        <p>
-            <strong class="text-lg">Age</strong>
-            <span class="text-xs text-gray-400 m-2 pt-2">
-                HCS pg. 28
-            </span>
-        </p>
-        <p>
-            Mapachs mature quickly, reaching adulthood around 8 years. Despite this they have fairly average lifespans, the oldest among them living just past 60 years.
-        </p>
-    </div>
-    {{--Alignment--}}
-    <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs mt-4 p-2">
-        <p>
-            <strong class="text-lg">Alignment</strong>
-            <span class="text-xs text-gray-400 m-2 pt-2">
-                HCS pg. 28
-            </span>
-        </p>
-        <p>
-            Mapachs don’t favor good over evil, with most choosing to remain neutral. They also lean towards the freedom of chaotic alignments.        </p>
-    </div>
-    {{--Size--}}
-    <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs mt-4 p-2">
-        <p>
-            <strong class="text-lg">Size</strong>
-            <span class="text-xs text-gray-400 m-2 pt-2">
-                HCS pg. 28
-            </span>
-        </p>
-        <p>
-            Mapachs stand between 4 and 5 feet tall, and encompass a variety of builds from svelt to wide, weighing between 90 and 160 pounds. Your size is Medium.
-        </p>
-    </div>
-    {{--Speed--}}
-    <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs mt-4 p-2">
-        <p>
-            <strong class="text-lg">Speed</strong>
-            <span class="text-xs text-gray-400 m-2 pt-2">
-                HCS pg. 28
-            </span>
-        </p>
-        <p>
-            Your base walking speed is 30 feet.
-        </p>
-    </div>
-    {{--Darkvission--}}
-    <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs mt-4 p-2">
-        <p>
-            <strong class="text-lg">Darkvision</strong>
-            <span class="text-xs text-gray-400 m-2 pt-2">
-                HCS pg. 28
-            </span>
-        </p>
-        <p>
-            You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can’t discern color in darkness, only shades of gray. Mapachs are most comfortable under the cloak of night.
-        </p>
-    </div>
-    {{--Resilience--}}
-    <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs mt-4 p-2">
-        <p>
-            <strong class="text-lg">Resilience</strong>
-            <span class="text-xs text-gray-400 m-2 pt-2">
-                HCS pg. 28
-            </span>
-        </p>
-        <p>
-            You have advantage on saving throws against poison and resistance against poison damage.
-        </p>
-    </div>
-    {{--Scroungecraft--}}
-    <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs mt-4 p-2">
-        <p>
-            <strong class="text-lg">Scroungecraft</strong>
-            <span class="text-xs text-gray-400 m-2 pt-2">
-                HCS pg. 28
-            </span>
-        </p>
-        <p>
-            You are proficient with tinker’s tools. Additionally, you have the ability to construct crude but functional versions of common items using materials present in your surroundings. You may spend 10 minutes to craft these materials into any tool or piece of adventuring gear worth 30 gold pieces or less. The item will be completely functional, even capable of passing for a disguise (if you crafted an article of clothing). Tools, along with any other item that would logically break on its first use (caltrops, arrows), will become useless afterward. Scroungecrafted items will otherwise last 1 hour before falling apart.
-        </p>
-        <p>
-            Should you have access to the proper materials, you can spend 8 hours converting an item you have scroungecrafted in this way into a permanent version, so long as you start this process before the item falls apart. You can also use scroungecraft to repair broken equipment, provided you have the materials on hand. Though, how long your repairs hold together is up to the GM.
-        </p>
-    </div>
-    {{--Skulker--}}
-    <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs mt-4 p-2">
-        <p>
-            <strong class="text-lg">Skulker</strong>
-            <span class="text-xs text-gray-400 m-2 pt-2">
+
+    <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs text-gray-400 p-2">
+        {{--Ability Score Increase--}}
+        <div>
+            <div class="dark:bg-gray-600 dark:border-gray-700 border-gray-200">
+                <p class="px-4 py-2">
+                    <strong class="text-lg">
+                        Ability Score Increase
+                    </strong>
+                    <span class="text-xs text-gray-400 m-2 pt-2">
+                    HCS, pg. 27
+                </span>
+                </p>
+            </div>
+            <div class="px-4 py-2 bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                <p class="py-1">
+                    Your Wisdom score increases by 2, and your Constitution score increases by 1.
+                </p>
+            </div>
+        </div>
+        {{--Age--}}
+        <div>
+            <div class="dark:bg-gray-600 dark:border-gray-700 border-gray-200">
+                <p class="px-4 py-2">
+                    <strong class="text-lg">
+                        Age
+                    </strong>
+                    <span class="text-xs text-gray-400 m-2 pt-2">
+                        HCS pg. 28
+                    </span>
+                </p>
+            </div>
+            <div class="px-4 py-2 bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                <p>
+                    Mapachs mature quickly, reaching adulthood around 8 years. Despite this they have fairly average lifespans, the oldest among them living just past 60 years.
+                </p>
+            </div>
+        </div>
+        {{--Alignment--}}
+        <div>
+            <div class="dark:bg-gray-600 dark:border-gray-700 border-gray-200">
+                <p class="px-4 py-2">
+                    <strong class="text-lg">
+                        Alignment
+                    </strong>
+                    <span class="text-xs text-gray-400 m-2 pt-2">
+                        HCS pg. 28
+                    </span>
+                </p>
+            </div>
+            <div class="px-4 py-2 bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                <p class="py-1">
+                    Mapachs don’t favor good over evil, with most choosing to remain neutral. They also lean towards the freedom of chaotic alignments.
+                </p>
+            </div>
+        </div>
+        {{--Size--}}
+        <div>
+            <div class="dark:bg-gray-600 dark:border-gray-700 border-gray-200">
+                <p class="px-4 py-2">
+                    <strong class="text-lg">
+                        Size
+                    </strong>
+                    <span class="text-xs text-gray-400 m-2 pt-2">
+                        HCS pg. 28
+                    </span>
+                </p>
+            </div>
+            <div class="px-4 py-2 bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                <p class="py-1">
+                    Mapachs stand between 4 and 5 feet tall, and encompass a variety of builds from svelt to wide, weighing between 90 and 160 pounds. Your size is Medium.
+                </p>
+            </div>
+        </div>
+        {{--Speed--}}
+        <dvi>
+            <div class="dark:bg-gray-600 dark:border-gray-700 border-gray-200">
+                <p class="px-4 py-2">
+                    <strong class="text-lg">
+                        Speed
+                    </strong>
+                    <span class="text-xs text-gray-400 m-2 pt-2">
+                         HCS pg. 28
+                    </span>
+                </p>
+            </div>
+            <div class="px-4 py-2 bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                <p class="py-1">
+                    Your base walking speed is 30 feet.
+                </p>
+            </div>
+        </dvi>
+        {{--Darkvission--}}
+        <div>
+            <div class="dark:bg-gray-600 dark:border-gray-700 border-gray-200">
+                <p class="px-4 py-2">
+                    <strong class="text-lg">
+                        Darkvision
+                    </strong>
+                    <span class="text-xs text-gray-400 m-2 pt-2">
+                        HCS pg. 28
+                    </span>
+                </p>
+            </div>
+            <div class="px-4 py-2 bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                <p class="py-1">
+                    You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can’t discern color in darkness, only shades of gray. Mapachs are most comfortable under the cloak of night.
+                </p>
+            </div>
+        </div>
+        {{--Resilience--}}
+        <div>
+            <div class="dark:bg-gray-600 dark:border-gray-700 border-gray-200">
+                <p class="px-4 py-2">
+                    <strong class="text-lg">
+                        Resilience
+                    </strong>
+                    <span class="text-xs text-gray-400 m-2 pt-2">
+                        HCS pg. 28
+                    </span>
+                </p>
+            </div>
+            <div class="px-4 py-2 bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                <p class="py-1">
+                    You have advantage on saving throws against poison and resistance against poison damage.
+                </p>
+            </div>
+        </div>
+        {{--Scroungecraft--}}
+        <div>
+            <div class="dark:bg-gray-600 dark:border-gray-700 border-gray-200">
+                <p class="px-4 py-2">
+                    <strong class="text-lg">
+                        Scroungecraft
+                    </strong>
+                    <span class="text-xs text-gray-400 m-2 pt-2">
+                        HCS pg. 28
+                    </span>
+                </p>
+            </div>
+            <div class="px-4 py-2 bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                <p class="py-1">
+                    You are proficient with tinker’s tools. Additionally, you have the ability to construct crude but functional versions of common items using materials present in your surroundings. You may spend 10 minutes to craft these materials into any tool or piece of adventuring gear worth 30 gold pieces or less. The item will be completely functional, even capable of passing for a disguise (if you crafted an article of clothing). Tools, along with any other item that would logically break on its first use (caltrops, arrows), will become useless afterward. Scroungecrafted items will otherwise last 1 hour before falling apart.
+                </p>
+                <p class="py-1">
+                    Should you have access to the proper materials, you can spend 8 hours converting an item you have scroungecrafted in this way into a permanent version, so long as you start this process before the item falls apart. You can also use scroungecraft to repair broken equipment, provided you have the materials on hand. Though, how long your repairs hold together is up to the GM.
+                </p>
+            </div>
+        </div>
+        {{--Skulker--}}
+        <div>
+            <div class="dark:bg-gray-600 dark:border-gray-700 border-gray-200">
+                <p class="px-4 py-2">
+                    <strong class="text-lg">
+                        Skulker
+                    </strong>
+                    <span class="text-xs text-gray-400 m-2 pt-2">
+                        HCS pg. 29
+                    </span>
+                </p>
+            </div>
+            <div class="px-4 py-2 bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                <p class="py-1">
+                    You have advantage on Stealth checks made in dim light and darkness.
+                </p>
+            </div>
+        </div>
+        {{--Languages--}}
+        <div>
+            <div class="dark:bg-gray-600 dark:border-gray-700 border-gray-200">
+                <p class="px-4 py-2">
+                    <strong class="text-lg">Languages</strong>
+                    <span class="text-xs text-gray-400 m-2 pt-2">
                 HCS pg. 29
             </span>
-        </p>
-        <p>
-            You have advantage on Stealth checks made in dim light and darkness.
-        </p>
-    </div>
-    {{--Languages--}}
-    <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs mt-4 p-2">
-        <p>
-            <strong class="text-lg">Languages</strong>
-            <span class="text-xs text-gray-400 m-2 pt-2">
-                HCS pg. 29
-            </span>
-        </p>
-        <p>
-            You can speak, read, and write Birdfolk and Mapach.
-        </p>
-    </div>
+                </p>
+            </div>
+            <div class="px-4 py-2 bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                <p class="py-1">
+                    You can speak, read, and write Birdfolk and Mapach.
+                </p>
+            </div>
+        </div>
 </div>
