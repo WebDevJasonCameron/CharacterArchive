1768c2e99a0cd7368132625cd4bdff2452e2d1e8
Jason Cameron <dev.jasoncameron@gmail.com>
2025-04-14 13:22:46 -0500

    spec and class updated

--- resources/views/character/components/content/view-container-cs-content-class.blade.php
+++ resources/views/character/components/content/view-container-cs-content-class.blade.php
@@ -7,19 +7,176 @@
     {{--Class--}}
     <div class="expand-toggle cursor-pointer text-blue-400 hover:text-blue-200 transition">
         <h5 class="text-xs text-gray-400 m-2 pt-2">
-            Druid
+            Druid Features
         </h5>
     </div>
+
+    {{--Content--}}
     <div class="expand-content bg-gray-500 rounded overflow-hidden shadow-md text-xs p-2">
-        <h7>
-            Ability Score Increase
-        </h7>
-        <p>
-            HCS, pg. 27
-        </p>
-        <p>
-            Your Wisdom score increases by 2, and your Constitution score increases by 1.
-        </p>
+        {{--Core Druid Traits--}}
+        <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs p-2">
+            <p>
+                <strong class="text-lg">Core Druid Traits </strong>
+                <span class="text-xs text-gray-400 m-2 pt-2">
+                   free-rules, pg. 79
+                </span>
+            </p>
+        </div>
+        {{--Spellcasting--}}
+        <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs p-2">
+            <p>
+                <strong class="text-lg">Spellcasting </strong>
+                <span class="text-xs text-gray-400 m-2 pt-2">
+                   free-rules, pg. 79
+                </span>
+            </p>
+        </div>
+        {{--Druidic--}}
+        <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs p-2">
+            <p>
+                <strong class="text-lg">Druidic </strong>
+                <span class="text-xs text-gray-400 m-2 pt-2">
+                   free-rules, pg. 80
+                </span>
+            </p>
+            <p>
+                You know Druidic and always have Speak with Animals prepared.
+            </p>
+            <p>
+                You can use Druidic to leave hidden message. You and others who know Druidic automatically spot such a message. Others spot the message’s presence with a successful DC 15 Intelligence (Investigation) check but can’t decipher it without magic.
+            </p>
+            <ul class="list-disc list-inside text-xs">
+                <li>
+                    Leave Druidic Message: (No Action)
+                </li>
+                <li>
+                    Speak with Animals(1st) (Ritual)
+                </li>
+            </ul>
+        </div>
+        {{--Primal Order--}}
+        <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs p-2">
+            <p>
+                <strong class="text-lg">Primal Order </strong>
+                <span class="text-xs text-gray-400 m-2 pt-2">
+                   free-rules, pg. 80
+                </span>
+            </p>
+            <p>
+                You have dedicated yourself to one of the sacred roles: <em>Magician:</em> You know one extra cantrip from the Druid spell list. You also gain a +3 bonus to Intelligence (Arcana or Nature) checks.
+            </p>
+        </div>
     </div>
 
+    {{--Class--}}
+    <div class="expand-toggle cursor-pointer text-blue-400 hover:text-blue-200 transition">
+        <h5 class="text-xs text-gray-400 m-2 pt-2">
+            Rogue Feature
+        </h5>
     </div>
+
+    {{--Content--}}
+    <div class="expand-content bg-gray-500 rounded overflow-hidden shadow-md text-xs p-2">
+        {{--Core Rogue Traits--}}
+        <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs p-2">
+            <p>
+                <strong class="text-lg">Core Rogue Traits </strong>
+                <span class="text-xs text-gray-400 m-2 pt-2">
+                   free-rules, pg. 129
+                </span>
+            </p>
+            <ul class="list-disc list-inside text-xs">
+                <li>Acrobatics</li>
+                <li>Insight</li>
+                <li>Stealth</li>
+                <li>Slight of Hand</li>
+            </ul>
+        </div>
+        {{--Expertise--}}
+        <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs p-2">
+            <p>
+                <strong class="text-lg">Expertise </strong>
+                <span class="text-xs text-gray-400 m-2 pt-2">
+                   free-rules, pg. 129
+                </span>
+            </p>
+            <p>
+                You gain Expertise in two of your skill proficiencies of your choice.
+            </p>
+            <ul class="list-disc list-inside text-xs">
+                <li>Stealth</li>
+                <li>Slight of Hand</li>
+            </ul>
+        </div>
+        {{--Sneak Attack--}}
+        <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs p-2">
+            <p>
+                <strong class="text-lg">Sneak Attack </strong>
+                <span class="text-xs text-gray-400 m-2 pt-2">
+                   free-rules, pg. 129
+                </span>
+            </p>
+            <p>
+                Once per turn you can deal an extra 1d6 damage to one creature you hit with an attack if you have Advantage on the roll and the attack uses a Finesse or Ranged weapon. The extra damage’s type is the same as the weapon’s type.
+            </p>
+            <p>
+                You don’t need Advantage on the attack if at least one of your allies is within 5 ft. of the target, the ally doesn’t have the Incapacitated condition, and you don’t have Disadvantage on the attack roll.
+            </p>
+            <ul class="list-disc list-inside text-xs">
+                <li>Sneak Attack: 1 Action</li>
+            </ul>
+        </div>
+        {{--Thieves' Cant--}}
+        <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs p-2">
+            <p>
+                <strong class="text-lg">Thieves' Cant </strong>
+                <span class="text-xs text-gray-400 m-2 pt-2">
+                   free-rules, pg. 129
+                </span>
+            </p>
+            <p>
+                You know Thieves’ Cant and one other language of your choice.
+            </p>
+            <ul class="list-disc list-inside text-xs">
+                <li>Celestial</li>
+            </ul>
+        </div>
+        {{--Weapon Mastery--}}
+        <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs p-2">
+            <p>
+                <strong class="text-lg">Weapon Mastery </strong>
+                <span class="text-xs text-gray-400 m-2 pt-2">
+                   PHB-2024, pg. 129
+                </span>
+            </p>
+            <p>
+                You know Thieves’ Cant and one other language of your choice.
+            </p>
+            <ul class="list-disc list-inside text-xs">
+                <li>
+                    <p>
+                        Dagger (Nick)
+                    </p>
+                    <p>
+                        Nick. When you make the extra attack of the Light property, you can make it as part of the Attack action instead of as a Bonus Action. This extra attack can only be made once per turn.
+                    </p>
+                    <p>
+                        Nick (Dagger): 1 Action
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        Hand Crossbow (Vex)
+                    </p>
+                    <p>
+                        Vex. If you hit a creature with a Hand Crossbow and deal damage to it, you have Advantage on your next attack roll against that creature before the end of your next turn.
+                    </p>
+                    <p>
+                        Vex (Hand Crossbow): 1 Action
+                    </p>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+</div>
--- resources/views/character/components/content/view-container-cs-content-specie.blade.php
+++ resources/views/character/components/content/view-container-cs-content-specie.blade.php
@@ -7,20 +7,133 @@
     aria-labelledby="species-features-tab">
 
     {{--Mapach--}}
-    <div class="expand-toggle cursor-pointer text-blue-400 hover:text-blue-200 transition">
+    <div>
         <h5 class="text-xs text-gray-400 m-2 pt-2">
             Mapach
         </h5>
     </div>
-    <div class="expand-content bg-gray-500 rounded overflow-hidden shadow-md text-xs p-2">
-        <strong class="text-lg">
-            Ability Score Increase
-        </strong>
-        <p class="text-xs text-gray-400 m-2 pt-2">
-            HCS, pg. 27
+
+    {{--Content--}}
+    {{--Ability Score Increase--}}
+    <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs p-2">
+        <p>
+            <strong class="text-lg">Ability Score Increase </strong>
+            <span class="text-xs text-gray-400 m-2 pt-2">
+                HCS, pg. 27
+            </span>
         </p>
         <p>
             Your Wisdom score increases by 2, and your Constitution score increases by 1.
+        </p>
+    </div>
+    {{--Age--}}
+    <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs mt-4 p-2">
+        <p>
+            <strong class="text-lg">Age</strong>
+            <span class="text-xs text-gray-400 m-2 pt-2">
+                HCS pg. 28
+            </span>
+        </p>
+        <p>
+            Mapachs mature quickly, reaching adulthood around 8 years. Despite this they have fairly average lifespans, the oldest among them living just past 60 years.
+        </p>
+    </div>
+    {{--Alignment--}}
+    <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs mt-4 p-2">
+        <p>
+            <strong class="text-lg">Alignment</strong>
+            <span class="text-xs text-gray-400 m-2 pt-2">
+                HCS pg. 28
+            </span>
+        </p>
+        <p>
+            Mapachs don’t favor good over evil, with most choosing to remain neutral. They also lean towards the freedom of chaotic alignments.        </p>
+    </div>
+    {{--Size--}}
+    <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs mt-4 p-2">
+        <p>
+            <strong class="text-lg">Size</strong>
+            <span class="text-xs text-gray-400 m-2 pt-2">
+                HCS pg. 28
+            </span>
+        </p>
+        <p>
+            Mapachs stand between 4 and 5 feet tall, and encompass a variety of builds from svelt to wide, weighing between 90 and 160 pounds. Your size is Medium.
+        </p>
+    </div>
+    {{--Speed--}}
+    <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs mt-4 p-2">
+        <p>
+            <strong class="text-lg">Speed</strong>
+            <span class="text-xs text-gray-400 m-2 pt-2">
+                HCS pg. 28
+            </span>
+        </p>
+        <p>
+            Your base walking speed is 30 feet.
+        </p>
+    </div>
+    {{--Darkvission--}}
+    <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs mt-4 p-2">
+        <p>
+            <strong class="text-lg">Darkvision</strong>
+            <span class="text-xs text-gray-400 m-2 pt-2">
+                HCS pg. 28
+            </span>
+        </p>
+        <p>
+            You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can’t discern color in darkness, only shades of gray. Mapachs are most comfortable under the cloak of night.
+        </p>
+    </div>
+    {{--Resilience--}}
+    <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs mt-4 p-2">
+        <p>
+            <strong class="text-lg">Resilience</strong>
+            <span class="text-xs text-gray-400 m-2 pt-2">
+                HCS pg. 28
+            </span>
+        </p>
+        <p>
+            You have advantage on saving throws against poison and resistance against poison damage.
+        </p>
+    </div>
+    {{--Scroungecraft--}}
+    <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs mt-4 p-2">
+        <p>
+            <strong class="text-lg">Scroungecraft</strong>
+            <span class="text-xs text-gray-400 m-2 pt-2">
+                HCS pg. 28
+            </span>
+        </p>
+        <p>
+            You are proficient with tinker’s tools. Additionally, you have the ability to construct crude but functional versions of common items using materials present in your surroundings. You may spend 10 minutes to craft these materials into any tool or piece of adventuring gear worth 30 gold pieces or less. The item will be completely functional, even capable of passing for a disguise (if you crafted an article of clothing). Tools, along with any other item that would logically break on its first use (caltrops, arrows), will become useless afterward. Scroungecrafted items will otherwise last 1 hour before falling apart.
+        </p>
+        <p>
+            Should you have access to the proper materials, you can spend 8 hours converting an item you have scroungecrafted in this way into a permanent version, so long as you start this process before the item falls apart. You can also use scroungecraft to repair broken equipment, provided you have the materials on hand. Though, how long your repairs hold together is up to the GM.
+        </p>
+    </div>
+    {{--Skulker--}}
+    <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs mt-4 p-2">
+        <p>
+            <strong class="text-lg">Skulker</strong>
+            <span class="text-xs text-gray-400 m-2 pt-2">
+                HCS pg. 29
+            </span>
+        </p>
+        <p>
+            You have advantage on Stealth checks made in dim light and darkness.
+        </p>
+    </div>
+    {{--Languages--}}
+    <div class="bg-gray-500 rounded overflow-hidden shadow-md text-xs mt-4 p-2">
+        <p>
+            <strong class="text-lg">Languages</strong>
+            <span class="text-xs text-gray-400 m-2 pt-2">
+                HCS pg. 29
+            </span>
+        </p>
+        <p>
+            You can speak, read, and write Birdfolk and Mapach.
         </p>
     </div>
 </div>
