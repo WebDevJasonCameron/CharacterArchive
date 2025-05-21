aef0913556235c7481498653e191e6179106e106
Jason Cameron <dev.jasoncameron@gmail.com>
2025-05-21 18:44:26 -0500

    adding mods

---
+++ app/Models/Character.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Notifications\Notifiable;
+
+class Character extends Model
+{
+    /** The attributes that are mass assignable
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'level',
+        'activeLevel',
+        'species',
+        'classes',
+        'backgroundSummary',
+        'fullHp',
+        'currentHp',
+        'ac',
+        'initiative',
+        'hitDice',
+        'proficiency',
+        'bonus',
+        'abilitySaves',
+        'xp',
+
+        'attributeStrength',
+        'attributeStrengthModifier',
+        'attributeDexterity',
+        'attributeDexterityModifier',
+        'attributeConstitution',
+        'attributeConstitutionModifier',
+        'attributeIntelligence',
+        'attributeIntelligenceModifier',
+        'attributeWisdom',
+        'attributeWisdomModifier',
+        'attributeCharisma',
+        'attributeCharismaModifier',
+
+        'skillAcrobatics',
+        'skillAcrobaticsProficiency',
+        'skillAnimalHandling',
+        'skillAnimalHandlingProficiency',
+        'skillArcana',
+        'skillArcanaProficiency',
+        'skillAthletics',
+        'skillAthleticsProficiency',
+        'skillDeception',
+        'skillDeceptionProficiency',
+        'skillHistory',
+        'skillHistoryProficiency',
+        'skillInsight',
+        'skillInsightProficiency',
+        'skillIntimidation',
+        'skillIntimidationProficiency',
+        'skillInvestigation',
+        'skillInvestigationProficiency',
+        'skillMedicine',
+        'skillMedicineProficiency',
+        'skillNature',
+        'skillNatureProficiency',
+        'skillPerception',
+        'skillPerceptionProficiency',
+        'skillPerformance',
+        'skillPerformanceProficiency',
+        'skillPersuasion',
+        'skillPersuasionProficiency',
+        'skillReligion',
+        'skillReligionProficiency',
+        'skillSleightOfHand',
+        'skillSleightOfHandProficiency',
+        'skillStealth',
+        'skillStealthProficiency',
+        'skillSurvival',
+        'skillSurvivalProficiency',
+
+        'proficienciesArmor',
+        'proficienciesWeapons',
+        'proficienciesTools',
+        'proficienciesLanguages',
+
+        'savingThrowsStrength',
+        'savingThrowsStrengthProficiency',
+        'savingThrowsDexterity',
+        'savingThrowsDexterityProficiency',
+        'savingThrowsConstitution',
+        'savingThrowsConstitutionProficiency',
+        'savingThrowsIntelligence',
+        'savingThrowsIntelligenceProficiency',
+        'savingThrowsWisdom',
+        'savingThrowsCharisma',
+        'savingThrowsCharismaProficiency',
+        'savingThrowsNotes',
+
+        'sensesPassivePerception',
+        'sensesPassiveInvestigation',
+        'sensesPassiveInsight',
+        'sensesNotes',
+
+        'defenses'
+    ];
+}
---
+++ app/Models/Spells.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Notifications\Notifiable;
+
+class Spells extends Model
+{
+    /** The attributes that are mass assignable
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'level',
+        'castingTime',
+        'rangeArea',
+        'componentVisual',
+        'componentSemantic',
+        'componentMaterial',
+        'componentMaterials',
+        'duration',
+        'concentration',
+        'ritual',
+        'school',
+        'saveType',
+        'description',
+        'imageUrl',
+        'source',
+        'tags',
+        'classAssociation'
+    ];
+}
