ef07b78a6c3f171127d40e381e5417debbc2afab
Jason Cameron <dev.jasoncameron@gmail.com>
2025-05-28 06:01:37 -0500

    continue to muddy up the db

--- 00_DB/sql/00_create_table_sql/52_ct_character_classes.sql
+++ 00_DB/sql/00_create_table_sql/52_ct_character_classes.sql
@@ -1,12 +1,11 @@
-CREATE TABLE IF NOT EXISTS character_class_features (
-                class_feature_id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS character_classes(
+                character_class_id SERIAL PRIMARY KEY,
                 character_id     INT NOT NULL,
-                class_name       VARCHAR(100) NOT NULL,
-                class_level      INT NOT NULL,
-                class_details    JSONB NOT NULL,
+                character_class_name       VARCHAR(100) NOT NULL,
+                character_class_level      INT NOT NULL,
+                character_class_details    JSONB NOT NULL,
 
     -- Optional Metadata
-                source_book      VARCHAR(255),
                 is_custom        BOOLEAN DEFAULT FALSE,
                 created_at       TIMESTAMPTZ DEFAULT NOW(),
                 updated_at       TIMESTAMPTZ DEFAULT NOW(),
--- 00_DB/sql/99_dlt_all.sql
+++ 00_DB/sql/99_dlt_all.sql
@@ -1,9 +1,36 @@
--- Drop Main Tables Second
-DROP TABLE IF EXISTS character_spells;
-DROP TABLE IF EXISTS character_details;
-DROP TABLE IF EXISTS inventory_items;
+-- Drop Junction Tables First
+DROP TABLE IF EXISTS item_types;
+DROP TABLE IF EXISTS item_tags;
+DROP TABLE IF EXISTS item_notes;
+DROP TABLE IF EXISTS item_conditions;
+DROP TABLE IF EXISTS item_sources;
+DROP TABLE IF EXISTS item_spells;
+DROP TABLE IF EXISTS item_effects;
 
+DROP TABLE IF EXISTS spell_tags;
+DROP TABLE IF EXISTS spell_conditions;
+DROP TABLE IF EXISTS spell_damagetypes;
+DROP TABLE IF EXISTS spell_classes;
+
+DROP TABLE IF EXISTS character_details;
+DROP TABLE IF EXISTS character_classes;
+DROP TABLE IF EXISTS characte_species;
+DROP TABLE IF EXISTS character_background;
+DROP TABLE IF EXISTS character_inventory;
+DROP TABLE IF EXISTS character_unique_items;
+DROP TABLE IF EXISTS character_spells;
+
+-- Drop Main Tables Second
+DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS types;
+DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS notes;
+DROP TABLE IF EXISTS conditions;
+DROP TABLE IF EXISTS damagetypes;
 DROP TABLE IF EXISTS spells;
+DROP TABLE IF EXISTS effects;
+DROP TABLE IF EXISTS rpg_classes;
+DROP TABLE IF EXISTS sources;
 DROP TABLE IF EXISTS characters;
 
 
