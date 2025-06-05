25e6492d32f9d9523ac558f3e33c63ccc678a473
Jason Cameron <dev.jasoncameron@gmail.com>
2025-06-04 19:43:25 -0500

    character spell

--- 00_DB/sql/03_insert_spell_data/00_insert_spells.sql
+++ 00_DB/sql/03_insert_spell_data/00_insert_spells.sql
@@ -1423,7 +1423,9 @@ Sample Creatures
        'abjuration',
        'none',
        'A creature you touch regains a number of Hit Points equal to 2d8 plus your spellcasting ability modifier.
-            Using a Higher-Level Spell Slot. The healing increases by 2d8 for each spell slot level above 1.'
+            Using a Higher-Level Spell Slot. The healing increases by 2d8 for each spell slot level above 1.',
+       '',
+       53
     ),(
        'Earth Tremor',
        '1st',
